1ea74e3f0741f0e0e88a83f995acf1642c6197c8
jeremyking1996 <73255406+jeremyking1996@users.noreply.github.com>
2020-11-17 14:17:14 -0500

    Step 2

--- myscripts.js
+++ myscripts.js
@@ -1,11 +1,20 @@
-let randomNum=Math.floor(Math.random() * Math.floor(100));
-let randomNum2=Math.floor(Math.random() * Math.floor(100));
+// let randomNum=Math.floor(Math.random() * Math.floor(100));
+// let randomNum2=Math.floor(Math.random() * Math.floor(100));
 
-let user=prompt(randomNum + " + " + randomNum2 + " Add the numbers and enter your answer")
+// let user=prompt(randomNum + " + " + randomNum2 + " Add the numbers and enter your answer")
 
-if(user==randomNum + randomNum2){
-alert ("You're right!")
-}
-else{
- alert ("Try again!")
-}
+// if(user==randomNum + randomNum2){
+// alert ("You're right!")
+// }
+// else{
+//  alert ("Try again!")
+// }
+
+// let userInput=prompt("Enter radius of a circle")
+
+// if (userInput>0) {
+//     alert(userInput**2 * (Math.PI));
+// }
+// else {
+//     alert("No negative numbers!!!")
+// }
